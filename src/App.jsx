44d66fb254d519b85ca8abdda591a0d82a8d2025
Moses Opiah <moefolder@Mosess-MacBook-Pro.local>
2024-02-06 20:48:53 +0000

    added nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, }  from 'react-router-dom';
 import './App.css'
 import Home from './components/Home'
+import NavigationBar from './components/Nav-bar'; 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Router>
+      <NavigationBar /> {}
         <Home />
         <Routes>
           <Route path="/react-portfolio/" element={<Home />} />
@@ -18,3 +21,4 @@ function App() {
 }
 
 export default App
+
---
+++ src/components/Nav-bar.jsx
@@ -0,0 +1,16 @@
+// NavBar.js
+
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
