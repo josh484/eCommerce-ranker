5af64b19d2eeb30075bea72e713c28a650395680
Moses Opiah <moefolder@Mosess-MacBook-Pro.local>
2024-02-07 20:58:38 +0000

    updated styling searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const SearchBar = () => {
   return (
     <form className="mt-3 mb-3">
-      <div className="input-group">
-        <input type="text" className="form-control" placeholder="Search..." />
-        <button type="submit" className="btn btn-primary">Search</button>
+      <div className="row justify-content-center">
+        <div className="col-sm-6">
+          <div className="input-group">
+            <input type="text" className="form-control" placeholder="Search..." />
+            <button type="submit" className="btn btn-primary">Search</button>
+          </div>
+        </div>
       </div>
     </form>
   );
