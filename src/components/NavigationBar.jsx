3259c1d6dd148a751eeb744a6cbf668011187765
Moses Opiah <moefolder@Mosess-MacBook-Pro.local>
2024-02-07 21:22:55 +0000

    updated styling on nav-bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,13 +4,14 @@ const NavigationBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
+        <a className="navbar-brand" href="/">E-Commerce Ranker</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="">Home</a>
+              <a className="nav-link" href="/">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/contact">Contact</a>
