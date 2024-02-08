12106f888094c942ff4fa3cfd82cb4869bc6c6a1
Moses Opiah <moefolder@Mosess-MBP.cust.communityfibre.co.uk>
2024-02-08 20:21:35 +0000

    added css to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import './Home.css';
 import Ranker from './Ranker'
 import SearchBar from './SearchBar';
 function Home() {
 
 
     return (
-        <>
-            <h1>Hello World</h1>        
+        <div className="home-container">
+            <h1>Welcome to the E-commerce Ranker</h1>        
             <SearchBar />
             <Ranker />
-        </>
+        </div>
     );
 }
 
-export default Home
+export default Home;
 
 
