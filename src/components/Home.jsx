8e9c2b02e258da5ad8a6d5e69230900aaa517404
Moses Opiah <moefolder@Mosess-MBP.cust.communityfibre.co.uk>
2024-02-08 20:49:32 +0000

    adding text to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
 
     return (
         <div className="home-container">
-            <h1>Welcome to the E-commerce Ranker</h1>        
+            <h1>Welcome to E-commerce Ranker, your go-to destination for smarter shopping decisions!</h1>        
             <SearchBar />
+            <p>At E-commerce Ranker, we understand the challenges consumers face when searching for the best deals online. That's why we've developed a powerful application that empowers users like you to take control of your shopping experience.</p>
             <Ranker />
         </div>
     );
