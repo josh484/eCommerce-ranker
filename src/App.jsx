ff1d7c617b749c8e913df68caeb77c7bfdf8d992
Josh484 <josh.m.mendoza1@gmail.com>
2024-02-08 18:18:06 +0000

    changed position of the searchbar to home

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes, }  from 'react-router-dom';
 import './App.css'
 import Home from './components/Home'
 import NavigationBar from './components/NavigationBar';
-import SearchBar from './components/SearchBar'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contact from './components/Contact';
 
@@ -14,7 +13,6 @@ function App() {
     <>
       <Router>
       <NavigationBar /> 
-      <SearchBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Ranker from './Ranker'
+import SearchBar from './SearchBar'; 
 function Home() {
 
 
     return (
         <>
             <h1>Hello World</h1>
+            <SearchBar />
             <Ranker />
         </>
     );
