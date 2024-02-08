5bbd015836566eedb72a36127becd67da0ed1067
Moses Opiah <moefolder@Mosess-MBP.cust.communityfibre.co.uk>
2024-02-08 21:01:18 +0000

    adding css to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './Home.css';
-import Ranker from './Ranker'
+import Ranker from './Ranker';
 import SearchBar from './SearchBar';
+
 function Home() {
-
-
     return (
         <div className="home-container">
-            <h1>Welcome to E-commerce Ranker, your go-to destination for smarter shopping decisions!</h1>        
-            <SearchBar />
-            <p>At E-commerce Ranker, we understand the challenges consumers face when searching for the best deals online. That's why we've developed a powerful application that empowers users like you to take control of your shopping experience.</p>
-            <Ranker />
+            <header className="header">
+                <h1>Welcome to E-commerce Ranker</h1>
+                <p className="sub-heading">Your go-to destination for smarter shopping decisions!</p>
+                <SearchBar />
+            </header>
+            <section className="main-content">
+                <p>At E-commerce Ranker, we understand the challenges consumers face when searching for the best deals online. That's why we've developed a powerful application that empowers users like you to take control of your shopping experience.</p>
+                <Ranker />
+            </section>
         </div>
     );
 }
 
 export default Home;
-
-
