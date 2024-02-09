67db333ed0aa3704c03c06ac0aa0ecdd167e197b
Josh484 <josh.m.mendoza1@gmail.com>
2024-02-09 18:15:18 +0000

    added usestate for search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = event => {
+    setSearch(event.target.value);
+
+  };
+
   return (
     <form className="mt-3 mb-3">
       <div className="row justify-content-center">
         <div className="col-sm-6">
           <div className="input-group">
-            <input type="text" className="form-control" placeholder="Search..." />
+            <input type="text" className="form-control" placeholder="Search..." onChange={handleSearch} value={search}/>
             <button type="submit" className="btn btn-primary">Search</button>
           </div>
         </div>
