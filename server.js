0c08f84d1e44e4dda20ded5927f4103abe042614
Enky <enkh6at@yahoo.com>
2024-02-13 00:15:43 +0000

    add middleware imports and initializing

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import express from "express";
 import multer from "multer";
 import { SpeechClient } from "@google-cloud/speech";
 
-
+const app = express();
+const upload = multer();
+const speechClient = new SpeechClient();
+app.use(cors());
 
 
 app.listen(5001, () => {
