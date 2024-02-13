399d77ef7a9a3e43179a62ed7a6af6fd7d3cc32a
Enky <enkh6at@yahoo.com>
2024-02-13 00:18:21 +0000

    create post route for speach to text conversion

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const upload = multer();
 const speechClient = new SpeechClient();
 app.use(cors());
 
+app.post("/convert-speech", upload.single("file"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+});
+
 
 app.listen(5001, () => {
   console.log("Server started on port 5001");
