0826c4b007ad266ff023c7ecd3cabdfa01171f01
KevinDsouza7 <victorkevindsouza7@gmail.com>
2024-02-14 08:54:35 +0000

    removed jumbotron

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './Home.css';
 import Ranker from './Ranker';
 import SearchBar from './SearchBar';
-import Jumbotron from './Jumbotron.js'; 
+
 function Home() {
   return (
     <div className="home-container">
-      <Jumbotron /> {}
+      
       <section className="main-content">
         <p>At E-commerce Ranker, we understand the challenges consumers face when searching for the best deals online. That's why we've developed a powerful application that empowers users like you to take control of your shopping experience.</p>
         <Ranker />
