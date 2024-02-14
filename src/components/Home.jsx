c78e0f37017118767b9e0e6033eff344f921bf1c
KevinDsouza7 <victorkevindsouza7@gmail.com>
2024-02-14 08:31:48 +0000

    added css for the homepage and navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import './Home.css';
 import Ranker from './Ranker';
 import SearchBar from './SearchBar';
-
+import Jumbotron from './Jumbotron.js'; 
 function Home() {
-    return (
-        <div className="home-container">
-            <header className="header">
-                <h1>Welcome to E-commerce Ranker</h1>
-                <p className="sub-heading">Your go-to destination for smarter shopping decisions!</p>
-            </header>
-            <section className="main-content">
-                <p>At E-commerce Ranker, we understand the challenges consumers face when searching for the best deals online. That's why we've developed a powerful application that empowers users like you to take control of your shopping experience.</p>
-                <Ranker />
-            </section>
-            
-        </div>
-    );
+  return (
+    <div className="home-container">
+      <Jumbotron /> {}
+      <section className="main-content">
+        <p>At E-commerce Ranker, we understand the challenges consumers face when searching for the best deals online. That's why we've developed a powerful application that empowers users like you to take control of your shopping experience.</p>
+        <Ranker />
+      </section>
+    </div>
+  );
 }
 
 export default Home;
